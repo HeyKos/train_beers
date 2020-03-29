31755696746107cfc6c0a33dbbb31f9f5e874f84
mkoser-andculture <mkoser@andculture.com>
2020-03-29 18:23:15 -0400

    Created Firebase cloud function to update the purchasedOn date for the current beer purchaser user. This effectively allows us to indicate which user is up to buy train beers next.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,39 +3,61 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
-export const addMessage = functions.https.onRequest(async (request, response) => {
-    const message = request.query.text;
-    // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    const snapshot = await admin.database().ref('/messages').push({original: message});
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    response.redirect(303, snapshot.ref.toString());
-    response.send("Hello from Firebase!");
-});
-
-export const makeUppercase = functions.database.ref("/messages/{pushId}/original").onCreate((snapshot, context) => {
-    // Grab the current value of what was written to the Realtime Database.
-    const original = snapshot.val();
-    console.log('Uppercasing', context.params.pushId, original);
-    const uppercase = original.toUpperCase();
-    // You must return a Promise when performing asynchronous tasks inside a Functions such as
-    // writing to the Firebase Realtime Database.
-    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-    return snapshot?.ref?.parent?.child('uppercase').set(uppercase);
-});
-
-export const setNextTrainBeerPerson = functions.firestore.document('/users/{id}').onUpdate((snapshot, context) => {
-    // Get the user that was updated, and if 
-});
-
 /*
-    TODO: Must pay for CloudScheduler on Firebase.
-    Runs at 5:00pm, every Friday, every month to determine who the next
-    person responsible for train beers is.
+    Runs at 5:00pm, every Friday, every month to update the purchasedOn timestamp for the user
+    that is currently set to purchase train beers.
 */
-export const scheduleNextTrainBeerPerson = functions.pubsub.schedule('0 0 17 ? * FRI *')
+export const updatePurchasedOnForCurrentUser = functions.pubsub.schedule('0 0 17 ? * FRI *')
     .timeZone('America/New_York')
     .onRun((context) => {
-        console.log('Runs 5:00pm, every Friday, every month');
-        return null;
+        // Get the active user who has gone the longest without buying train beers.
+        getCurrentBeerPurchaser()
+            .then(onGetCurrentBeerPurchaserSuccess)
+            .catch((e) => {
+                console.error("An error occurred while getting the current purchaser user.", e);
+            });
     }
 );
+
+/*
+    Get the user in Firestore that is set to buy beer. This is based on the active user that has
+    gone the longest time buying beer. 
+*/
+function getCurrentBeerPurchaser(): Promise<FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>> {
+    return admin.firestore()
+        .collection("users")
+        .where("isActive", "==", true)
+        .orderBy("purchasedOn", "asc")
+        .limit(1)
+        .get();
+}
+
+/*
+    Calls a method to update the purchasedOn field when a user is successfully retrieved from Firestore.
+*/
+function onGetCurrentBeerPurchaserSuccess(snapshot: FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>) {
+    if (snapshot.docs.length === 0) {
+        console.error("No active users found!");
+        return;
+    }
+
+    const user: FirebaseFirestore.DocumentData = snapshot.docs[0];
+    // Update the user purchasedOn
+    updateUserPurchasedOn(user);
+}
+
+/*
+    Updates a user document in Firestore to set the purchasedOn field to now.
+*/
+function updateUserPurchasedOn(user: FirebaseFirestore.DocumentData) {
+    admin.firestore()
+        .collection("users")
+        .doc(user.id)
+        .update("purchasedOn", admin.firestore.Timestamp.now())
+        .then((result:FirebaseFirestore.WriteResult) => {
+            console.log(`Updated user: ${user.get("name")} (${user.id}) in: ${result.writeTime.seconds} (sec).`)
+        })
+        .catch((e) => {
+            console.error("An error occurred when updating the document.", e);
+        });
+}
