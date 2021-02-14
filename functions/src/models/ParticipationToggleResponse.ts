ebd24e1ab0533c696504fcfed1a69c9a9a1c5d90
mikekoser <mikekoser@gmail.com>
2021-02-14 16:29:23 -0500

    Refactored the toggle_event_participation file to break things down into separate functions.

---
+++ functions/src/models/ParticipationToggleResponse.ts
@@ -0,0 +1,5 @@
+export interface ParticipationToggleResponse {
+    eventId: string;
+    isParticipating?: boolean;
+    userId: string;
+}
--- functions/src/toggle_event_participation.ts
+++ functions/src/toggle_event_participation.ts
@@ -1,81 +1,121 @@
 import * as admin from 'firebase-admin';
-import { ParticipationToggleResponse } from './models/participation_toggle_response';
+import { EventParticipant } from './models/EventParticipant';
+import { ParticipationToggleResponse } from './models/ParticipationToggleResponse';
 
-// Type aliases
+// ------------------------
+// Type Aliases
+// ------------------------
+
 type DocData = FirebaseFirestore.DocumentData;
 type DocRef = FirebaseFirestore.DocumentReference<DocData>;
 type WriteResult = FirebaseFirestore.WriteResult;
 
-// Interfaces
-interface EventParticipant {
-    event: DocRef,
-    user: DocRef,
-}
+
+// ------------------------
+// Exported Functions
+// ------------------------
 
 export const toggleEventParticipation = async (userId: string): Promise<ParticipationToggleResponse> => {
-    // In order to test we're going to use hard-coded event and user data.
-    // These will eventually be replaced with parameters, or API calls.
-    // Get current event
-    // TODO: Replace with API call
-    const eventId: string = "SeH13v22sQ0CSJywrtm8";
     const response: ParticipationToggleResponse = {
-        eventId: eventId,
-        userId: userId,
+        eventId: "",
+        userId: "",
     };
-    // TODO: Break this out into a separate function
-    const eventRef: DocRef = admin.firestore().collection("events").doc(eventId);
-    if (eventRef === null || eventRef === undefined) {
+
+    const eventRef = await getEventRef();
+    if (eventRef === null) {
         return response;
     }
-    // Get user
-    // TODO: Break this out into a separate function
-    const userRef: DocRef = admin.firestore()
-        .collection("users")
-        .doc(userId);
-    if (userRef === null || userRef === undefined) {
+    response.eventId = eventRef.id;
+
+    const userRef = getUserRef(userId);
+    if (userRef === null) {
         return response;
     }
-    // Get event participation for user
-    // TODO: Break this out into a separate function
-    const getParticipantResult: DocData = await admin.firestore()
+    response.userId = userRef.id;
+
+    const participantResult = await getParticipant(eventRef, userRef);
+
+    // If no event participation record exists, create one to indicate the user
+    // is participating in the event.
+    if (participantResult.docs === null || participantResult.docs.length === 0) {
+        await createParticipant(eventRef, userRef);
+        response.isParticipating = true;
+        return response;
+    }
+
+    // if record exists, delete it to indicate the user is not participating
+    // in the event.
+    const participant = participantResult.docs[0];
+    await deleteParticipant(participant.id);
+    response.isParticipating = false;
+
+    return response;
+};
+
+
+// ------------------------
+// Private Functions
+// ------------------------
+
+const createParticipant = async (eventRef: DocRef, userRef: DocRef) => {
+    const newParticipant: EventParticipant = {
+        event: eventRef,
+        user: userRef,
+    };
+
+    await admin.firestore()
+        .collection("event_participants")
+        .add(newParticipant)
+        .then(() => {
+            console.log(`An new event participant was added. eventId: ${ eventRef.id } | userId: ${ userRef.id }`);
+        })
+        .catch((e) => {
+            console.error("An error occurred when creating event.", e);
+        });
+};
+
+const deleteParticipant = async (participantId: string): Promise<void> => {
+    const deleteParticipantResult: WriteResult = await admin.firestore()
+    .collection("event_participants")
+    .doc(participantId)
+    .delete();
+
+    const deletedOn = deleteParticipantResult.writeTime.toDate().toString();
+    const message = `Participant ${ participantId } deleted at ${ deletedOn }`;
+    console.log(message);
+};
+
+const getCurrentEvent = async (): Promise<DocData|null> => {
+    const getEventResult: DocData = await admin.firestore()
+        .collection("events")
+        .orderBy("date", "desc")
+        .limit(1)
+        .get();
+
+    if (getEventResult.length === 0) {
+        return null;
+    }
+
+    return getEventResult.docs[0];
+};
+
+const getEventRef = async ():Promise<DocRef|null> => {
+    const eventData = await getCurrentEvent();
+    if (eventData === null) {
+        return null;
+    }
+
+    return admin.firestore().collection("events").doc(eventData.id);
+};
+
+const getParticipant = async (eventRef: DocRef, userRef: DocRef)
+    : Promise<DocData> =>
+    await admin.firestore()
         .collection("event_participants")
         .where("event", "==", eventRef)
         .where("user", "==", userRef)
         .limit(1)
         .get();
 
-    // If no event participation record exists create one to indicate the user is particpating in the event.
-    if (getParticipantResult.docs.length === 0) {
-        // TODO: Break this out into a separate function
-        const newParticipant: EventParticipant = {
-            event: eventRef,
-            user: userRef,
-        };
-        await admin.firestore()
-            .collection("event_participants")
-            .add(newParticipant)
-            .then(() => {
-                console.log(`An new event participant was added. eventId: ${ eventRef.id } | userId: ${ userRef.id }`);
-            })
-            .catch((e) => {
-                console.error("An error occurred when creating event.", e);
-            });
-
-        response.isParticipating = true;
-        return response;
-    }
-
-    // if record exists, delete it to indicate the user is not particpating in the event
-    // TODO: Break this out into a separate function
-    const participant = getParticipantResult.docs[0];
-    const deleteParticipantResult: WriteResult = await admin.firestore()
-        .collection("event_participants")
-        .doc(participant.id)
-        .delete();
-
-    const deletedOn = deleteParticipantResult.writeTime.toDate().toString();
-    const message = `Participant ${ participant.id } deleted at ${ deletedOn }`;
-    console.log(message);
-    response.isParticipating = false;
-    return response;
-};
+const getUserRef = (userId: string): DocRef|null =>
+    admin.firestore().collection("users").doc(userId);
